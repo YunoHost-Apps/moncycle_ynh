68e298d723bfb72123757770eaa8b62075562c20
Limezy <arnaudetpia@de-ruffray.fr>
2023-01-20 02:02:20 +0700

    First work

---
+++ conf/config.php
@@ -0,0 +1,25 @@
+<?php
+/* moncycle.app
+**
+** licence Creative Commons CC BY-NC-SA
+**
+** https://www.moncycle.app
+** https://github.com/jean-io/moncycle.app
+*/
+
+define("DB_HOST", "");
+define("DB_ID", "__APP__");
+define("DB_NAME", "__APP__");
+define("DB_PORT", 3306);
+define("DB_PASSWORD", "");
+
+define("SMTP_HOST", 'localhost');
+define("SMTP_PORT", 25);
+define("SMTP_MAIL", "__APP__@__DOMAIN__");
+define("SMTP_PASSWORD", "");
+
+define("CREATION_COMPTE", true);
+define("CONNEXION_COMPTE", true);
+
+define("CSV_SEP", ";");
+
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,16 +12,16 @@
 # This behaviour can be overridden by setting the YNH_PHP_VERSION variable
 #YNH_PHP_VERSION=7.3
 #YNH_PHP_VERSION=7.4
-#YNH_PHP_VERSION=8.0
+YNH_PHP_VERSION=8.0
 # For more information, see the PHP application helper: https://github.com/YunoHost/yunohost/blob/dev/helpers/php#L3-L6
 # Or this app package depending on PHP: https://github.com/YunoHost-Apps/grav_ynh/blob/master/scripts/_common.sh
 # PHP dependencies used by the app (must be on a single line)
 #php_dependencies="php$YNH_PHP_VERSION-deb1 php$YNH_PHP_VERSION-deb2"
 # or, if you do not need a custom YNH_PHP_VERSION:
-php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+php_dependencies="php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-pdo php$YNH_DEFAULT_PHP_VERSION-pdo_mysql"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="deb1 deb2 $php_dependencies"
+pkg_dependencies="libfreetype6-dev libjpeg62-turbo-dev libpng-dev $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
