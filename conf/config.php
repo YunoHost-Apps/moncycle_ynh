6988fa00a96e692a13a0de74214e9507ca72b24a
Limezy <arnaudetpia@de-ruffray.fr>
2023-01-20 23:52:32 +0700

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -7,13 +7,13 @@
 ** https://github.com/jean-io/moncycle.app
 */
 
-define("DB_HOST", "");
+define("DB_HOST", "localhost");
 define("DB_ID", "__APP__");
 define("DB_NAME", "__APP__");
 define("DB_PORT", 3306);
 define("DB_PASSWORD", "__DB_PWD__");
 
-define("SMTP_HOST", 'localhost');
+define("SMTP_HOST", "localhost");
 define("SMTP_PORT", 25);
 define("SMTP_MAIL", "__APP__@__DOMAIN__");
 define("SMTP_PASSWORD", "");
