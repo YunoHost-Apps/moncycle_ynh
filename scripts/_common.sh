650e76ab4342839b9301283080d681e07e3d3f9b
Limezy <arnaudetpia@de-ruffray.fr>
2023-01-22 14:22:24 +0700

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,10 @@
 # PHP APP SPECIFIC
 #=================================================
 
-YNH_PHP_VERSION=7.4
+php_dependencies="php$YNH_DEFAULT_PHP_VERSION-fpm php$YNH_DEFAULT_PHP_VERSION-cli php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-mysql"
 
 # dependencies used by the app (must be on a single line)
+
 pkg_dependencies="libfreetype6-dev libjpeg62-turbo-dev libpng-dev $php_dependencies"
 
 #=================================================
