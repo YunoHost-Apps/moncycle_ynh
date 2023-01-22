128f4e3c77f29ad4b4dc52fb9678b17a0d9ada55
Limezy <arnaudetpia@de-ruffray.fr>
2023-01-22 14:16:20 +0700

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 # PHP APP SPECIFIC
 #=================================================
 
-YNH_PHP_VERSION=8.0
+YNH_PHP_VERSION=7.4
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="libfreetype6-dev libjpeg62-turbo-dev libpng-dev $php_dependencies"
